f92b91308dc28e9aa51ff743711da6e2a45b4f47
ObernhumerMichael <michaelobernhumer@gmail.com>
2025-03-26 15:55:55 +0100

    feat: added basic db schema

--- postgres/init/init.sql
+++ postgres/init/init.sql
@@ -1,5 +1,22 @@
--- Create schema
-CREATE SCHEMA task_manager;
+-- Create the tasks table
+DROP TABLE IF EXISTS tasks;
+CREATE TABLE tasks
+(
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    status VARCHAR(50) DEFAULT 'pending',
+    -- can be 'pending', 'in-progress', or 'completed'
+    due_date DATE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
--- Use schema
-SET search_path TO task_manager;
+-- Add example task
+INSERT INTO tasks
+    (title, description, status, due_date)
+VALUES
+    ('Hello World',
+        'This is an example task.',
+        'pending',
+        '2099-01-01');
