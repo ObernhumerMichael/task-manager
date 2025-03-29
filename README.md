e91922b174eae5beb83f6c58dd2ace4ac3b43943
ObernhumerMichael <michaelobernhumer@gmail.com>
2025-03-29 10:20:10 +0100

    feat: added insert form

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 
 This is a simple Task Manager webapp that I creat with the sole purpose to learn PHP. 
 Please do NOT use this in a production environment!
+
+# Dev
+
+Start the project:
+```docker compose up -d --build```
+
+Stop the project:
+```docker compose down -v```
--- php/index.php
+++ php/index.php
@@ -10,6 +10,17 @@
 
 <body>
     <h1>Task Manager</h1>
+    <div id="insert">
+        <form action="insert.php" method="post">
+            <label for="title">Title:</label>
+            <input type="text" name="title" id="title" placeholder="Write the task title here...">
+            <label for="due_dat">Due Date:</label>
+            <input type="datetime-local" name="due_date" id="due_date">
+            <label for="description">Description:</label>
+            <textarea name="description" id="description" placeholder="Write the details here..."></textarea>
+            <input type="submit" value="Add Task">
+        </form>
+    </div>
     <div id="tasks">
         <?php
         require_once "db.php";
